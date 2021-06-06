1394cf705ccbe6c8ae7fa6e300afc8d3fbb38d4b
Erdong Chen <erdong1993@gmail.com>
2021-06-06 14:56:17 -0700

    Level 2: added md5 hashed password

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require ("express");
 const bodyparser = require ("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
+// const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -23,7 +24,7 @@ const userSchema = new mongoose.Schema({
 });
 
 // const secret = "Thisisascerect.";
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
+// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 
 const User = new mongoose.model("User", userSchema);
@@ -43,7 +44,7 @@ app.get("/register", function(req,res){
 app.post("/register", function(req,res){
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
   newUser.save(function(err){
     if (!err){
@@ -56,7 +57,7 @@ app.post("/register", function(req,res){
 
 app.post("/login", function(req,res){
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({email: username}, function(err, foundUser){
     if (err) {
