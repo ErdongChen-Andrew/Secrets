f5f3a2b914b9d77f02a7243e7b7f0ae5dc309c17
Erdong Chen <erdong1993@gmail.com>
2021-06-05 22:05:52 -0700

    Level 2: added enviroment vars

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require ("express");
 const bodyparser = require ("body-parser");
 const ejs = require("ejs");
@@ -5,6 +6,8 @@ const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
 
 const app = express();
+
+// console.log(process.env.SECRET);
 
 app.use(express.static("public"));
 app.set("view engine","ejs");
@@ -19,8 +22,8 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-const secret = "Thisisascerect.";
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+// const secret = "Thisisascerect.";
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 
 const User = new mongoose.model("User", userSchema);
